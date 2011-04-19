93eb07acd9be916a6aba6dfc3117481b3aa9b1a4
Scott <scott@Verity.(none)>
2011-04-19 15:38:24 -0500

    Did static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,22 @@
 class PagesController < ApplicationController
   def home
+  	@title = "Home"
   end
 
   def sadmin
+  	@title = "Survey Administration"
   end
 
   def qadmin
+  	@title = "Question Administration"
   end
 
   def take
+  	@title = "Take Survey"
   end
 
   def results
+  	@title = "View Results"
   end
 
 end
