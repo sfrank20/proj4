79f3b157b806a6002843a90541548605f0493c89
Scott <sfrank20@uic.edu>
2011-04-19 22:01:20 -0500

    layout fixes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,50 @@
+class UsersController < ApplicationController
+
+  def show
+  	@user = User.find(params[:id])
+  	@title = @user.name
+  end
+  
+  def new
+  	@user = User.new
+  	@title = "Sign up"
+  end
+  
+  def create
+  	@user = User.new(params[:user])
+  	if @user.save
+  		flash[:success] = "Welcome to the Sample App!"
+  		redirect_to @user
+  	else
+  		@title = "Sign up"
+  		render 'new'
+  	end
+  end
+  
+  def edit
+  	@user = User.find(params[:id])
+  	@title = "Edit user"
+  end
+  
+  def update
+  	@user = User.find(params[:id])
+  	if @user.update_attributes(params[:user])
+  		flash[:success] = "Profile updated."
+  		redirect_to @user
+  	else
+  		@title = "Edit user"
+  		render 'edit'
+  	end
+  end
+
+  def index
+  	@title = "All users"
+  	@users = User.all
+  end
+  
+  def show
+  	@user = User.find(params[:id])
+  	@title = @user.name
+  end
+
+end
